9d4e39550fe3be7ab7f21a48b3bca7a21493fe5b
nayakabinashnsp <nayakabinashnsp@gmail.com>
2023-07-15 11:24:24 +0530

    my greateOutDoor

---
+++ src/main/java/com/capgemini/go/GreatoutdoorsApplication.java
@@ -0,0 +1,11 @@
+package com.capgemini.go;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatoutdoorsApplication {
+
+	public static void main(String[] args) { }
+
+}
---
+++ src/main/webapp/frontend/src/data.js
@@ -0,0 +1,110 @@
+
+
+export const sliderItems =[
+      {
+        id:1,
+        img:"https://cdn.pixabay.com/photo/2022/01/27/06/52/woman-6971274_1280.png",
+        title:"SUMMER SALE",
+        desc:"DON'T COMPRISION ON STYLE GET FLATE 30% OFF GET FLATE NEW ARRIVAL",
+        bg:"f5fafd",
+      },
+      {
+        id:2,
+        img:"https://cdn.pixabay.com/photo/2021/10/27/06/05/boys-6746056_1280.png",
+        title:"SUMMER SALE",
+        desc:"DON'T COMPRISION ON STYLE GET FLATE 30% OFF GET FLATE NEW ARRIVAL",
+        bg:"fsfafd",
+      },
+      {
+        id:3,
+        img:"https://cdn.pixabay.com/photo/2021/10/27/06/05/boys-6746056_1280.png",
+        title:"SUMMER SALE",
+        desc:"DON'T COMPRISION ON STYLE GET FLATE 30% OFF GET FLATE NEW ARRIVAL",
+        bg:"fbf0f4",
+      }
+
+    ]
+    export const categories =[
+
+        {
+            id:1,
+            img:"https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGxhcHRvcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+            title:"THINK-PAD"
+        },
+        {
+            id:2,
+            img:"https://images.unsplash.com/photo-1612817159949-195b6eb9e31a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+            title:" STYLE WATCH"
+        },
+        {
+            id:3,
+            img:"https://media.istockphoto.com/id/1296540708/photo/beautiful-woman-enjoying-autumn-day-in-nature.jpg?b=1&s=170667a&w=0&k=20&c=ik9P0EUveFt-xiftlDci7mQHhHAVD_RnLFMojCOx4ec=",
+            title:"LONGWEAR LOVE"
+        }
+    ]
+
+    export const popularProduct=[
+
+        {
+          id:1,
+          img:"https://png.pngtree.com/png-clipart/20220718/ourmid/pngtree-formal-mens-sky-blue-check-shirt-with-pant-free-png-and-png-image_6005528.png",
+
+        },
+
+        {
+          id:2,
+          img:"https://pngimg.com/uploads/suit/small/suit_PNG93243.png ",
+
+        },
+        {
+          id:3,
+          img:"https://png.pngitem.com/pimgs/s/478-4786769_men-t-shirt-png-image-t-shirt-png.png",
+
+        },
+        {
+          id:4,
+          img:"https://png.pngitem.com/pimgs/s/478-4786766_j-crew-mercantile-field-jacket-hd-png-download.png ",
+
+        },
+        {
+          id:5,
+          img:"https://png.pngitem.com/pimgs/s/174-1747324_jeans-for-men-png-diesel-jeans-2019-larkee.png",
+
+        },
+        {
+          id:6,
+          img:"https://png.pngitem.com/pimgs/s/148-1483418_mens-star-wars-dress-shirt-hd-png-download.png",
+
+        },
+        {
+          id:7,
+          img:"https://pngimg.com/uploads/suit/small/suit_PNG8126.png ",
+
+        },
+        {
+          id:8,
+          img:"https://assets.stickpng.com/thumbs/580b57fbd9996e24bc43bef1.png",
+
+        },
+        {
+          id:9,
+          img:" https://png.pngitem.com/pimgs/s/334-3341387_armor-express-dress-vest-ce-men-s.png",
+
+        },
+
+        {
+          id:10,
+          img:" https://png.pngitem.com/pimgs/s/189-1897357_mens-ostrich-dress-boots-hd-png-download.png",
+
+        }
+
+
+     
+
+
+
+
+
+
+
+    ]
